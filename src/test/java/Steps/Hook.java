83d85b39e09902bb6d8ee68c9a85f6eeb3b2b7af
Waqas Khan <68295195+waqa5kh4n@users.noreply.github.com>
2021-03-19 10:32:40 -0400

    Update Hook.java

--- src/test/java/Steps/Hook.java
+++ src/test/java/Steps/Hook.java
@@ -13,6 +13,10 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 import com.google.common.io.Files;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
@@ -26,22 +30,58 @@ public class Hook {
 	@Before
 	public void startSetUp() {
 
-		System.setProperty("webdriver.chrome.silentOutput", "true");
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("enable-automation");
-		//options.addArguments("--headless");
-		options.addArguments("--window-size=1920,1080");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-extensions");
-		options.addArguments("--dns-prefetch-disable");
-		options.addArguments("--disable-gpu");
-		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-		options.addArguments("enable-features=NetworkServiceInProcess");
-		options.addArguments("disable-features=NetworkService");
-		driver = new ChromeDriver(options);
-		driver.manage().window().setSize(new Dimension(1200, 800));
+//		System.setProperty("webdriver.chrome.silentOutput", "true");
+//		WebDriverManager.chromedriver().setup();
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("enable-automation");
+//		//options.addArguments("--headless");
+//		options.addArguments("--window-size=1920,1080");
+//		options.addArguments("--no-sandbox");
+//		options.addArguments("--disable-extensions");
+//		options.addArguments("--dns-prefetch-disable");
+//		options.addArguments("--disable-gpu");
+//		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+//		options.addArguments("enable-features=NetworkServiceInProcess");
+//		options.addArguments("disable-features=NetworkService");
+//		driver = new ChromeDriver(options);
+//		driver.manage().window().setSize(new Dimension(1200, 800));
+//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		driver.manage().deleteAllCookies();
+//		File folder = new File(System.getProperty("user.dir") + "/FailedScreenshots/");
+//		for (File f : folder.listFiles()) {
+//			if (f.getName().endsWith(".png")) {
+//				f.delete();
+//			}
+//		 }
+		
+		
+		String osName = System.getProperty("os.name");
+		if (osName.equalsIgnoreCase("Mac OS X")) {
+		
+				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/Drivers/chromedriver");
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--disable-notifications");
+
+				driver = new ChromeDriver(options);
+
+		} else if (osName.equals("Windows 10")) {
+
+				System.setProperty("webdriver.chrome.driver",
+						System.getProperty("user.dir") + "/Drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+
+		} else if (osName.equals("Linux")) {
+			DesiredCapabilities caps = new DesiredCapabilities();
+			caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
+			caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+					System.getProperty("user.dir") + "/Drivers/phantomjs");
+			driver = new PhantomJSDriver(caps);
+		} else {
+			System.out.println("Driver is not configured for this Operating System.");
+		}
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 		driver.manage().deleteAllCookies();
 		File folder = new File(System.getProperty("user.dir") + "/FailedScreenshots/");
 		for (File f : folder.listFiles()) {
@@ -49,6 +89,23 @@ public class Hook {
 				f.delete();
 			}
 		 }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
 	}
 
 	@After
